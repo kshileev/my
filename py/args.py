036cf600b308b20bad7d35d99109ae2623daa842
Kirill Shileev <kshileev@gmail.com>
2015-06-10 09:09:00 -0700

    add ArgumentParser exmple

---
+++ py/args.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--foo', required=False, help='foo help')
+parser.add_argument('--all', action='store_true', help='all help')
+args = parser.parse_args()
+print args
