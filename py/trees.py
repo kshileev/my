0852212888b72388810c77700307578277a8f1d0
Kirill Shileev <kshileev@gmail.com>
2016-02-09 12:44:13 +0300

    tree operations

--- py/trees.py
+++ py/trees.py
@@ -1,40 +1,92 @@
+class TreeNode:
+    def __init__(self, uid, parent, children):
+        self.uid = uid
+        self.parent = parent
+        self.children = children
 
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
+    def to_list(self):
 
-    def traverse(self, l_values=None):
-        if l_values:
-            l_values.append(self.value)
+        if values:
+            values.append(self.uid)
         else:
             l_values = [self.value]
-        if self.left:
-            self.left.traverse(l_values)
-        if self.right:
-            self.right.traverse(l_values)
+        for child in self.children:
+            child.traverse(l_values)
         return l_values
 
+    def list_of_parents(self):
+        """Traverse the tree to the root and build a list of nodes seen"""
+        if not self.parent:
+            return [self] + self.parent.list_of_parents()
+        else:
+            return None
+
     @staticmethod
-    def _reconstruct(l_values, start, end):
-        root = Node(l_values[start])
+    def least_common_parents(node1, node2):
+        if node1 == node2:
+            return node1
+        l1 = node1.list_of_parents()  #log(N)
+        l2 = node2.list_of_parents()
+
+        min_len = min(len(l1), len(l2))
+
+        i = 0
+        for i in range(min_len):
+            while not l1[-i] == l2[-i]:
+                continue
+        return l1[i+1]
+
+    @staticmethod
+    def build_binary_from_list(lst, start=None, end=None):
+        start = start or 0
+        end = end or len(values)
+
+        root = Node(values[start])
         left_start = start + 1
         right_start = left_start
-        while right_start < end and l_values[right_start] < root.value:
+        while right_start < end and values[right_start] < root.value:
             right_start += 1
 
         if left_start < right_start:
-            root.left = Node._reconstruct(l_values, left_start, right_start)
+            root.left = Node.build_binary_from_list(values, left_start, right_start)
         if right_start < end:
-            root.right = Node._reconstruct(l_values, right_start, end)
+            root.right = Node.build_binary_from_list(values, right_start, end)
         return root
 
-    @staticmethod
-    def reconstruct(l_values):
-        return Node._reconstruct(l_values, 0, len(l_values))
+
+class Tree:
+    (ROOT, DEPTH, BREADTH) = range(3)
+
+    def __init__(self):
+        self.__nodes = {}
+
+
+    def print(self, identifier, depth=ROOT):
+        children = self[identifier].children
+        if depth == _ROOT:
+            print('{0}'.format(identifier))
+        else:
+            print("\t"*depth, "{0}".format(identifier))
+
+        depth += 1
+        for child in children:
+            self.print(child, depth)  # recursive call
+
+    def __getitem__(self, item):
+        return self.__nodes[item]
+
+    def __setitem__(self, key, value):
+        self.__nodes[key] = value
 
 if __name__ == '__main__':
-    tree = Node.reconstruct([10, 5, 3, 7, 15, 12, 17, 19])
+    """    a
+          / \
+         b   c
+             / \
+            d   f
+             \
+              e
+    """
+    tree = Node.build_binary_from_list(['a', 'b', 'c', 'd', 15, 12, 17, 19])
     values = tree.traverse()
     print values
