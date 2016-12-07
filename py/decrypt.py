9e4657479c81eb4a09ce16bd6731e7f5e951ce1a
Kirill Shileev <kshileev@gmail.com>
2016-12-07 17:21:33 +0300

    decrypt: added caeser cipher

--- py/decrypt.py
+++ py/decrypt.py
@@ -1,46 +1,78 @@
-import unittest
-"""This is a sample -> histsiaampels
-[This, is,sample, a, additional]
-"""
+from unittest import TestCase
 
-WORLDS = ['This', 'is', 'a', 'sample', 'first', 'second']
 
-def scramble(message):
-	"""shift all letters in message into a random position
-	:param message: initial message
-	"""
-	import random
+class Decipher:
 
-	scrambled_message = list(message)
-	message_len = len(message)
-	for i, letter in enumerate(message):
-		k = random.randint(0, len(message))
-		message[k] = letter
-	return scrambled_message
+    ENGLISH_WORLDS = ['This', 'is', 'a', 'sample', 'first', 'second']
 
-def words_in_message(message, words)
-	found_words = [word for word in words if word in message] # O(len(words))
-	return  sum(map(lambda word: len(word), found_words)), found_words
+    @staticmethod
+    def caesar_cipher(message, encryption_key):
+        """ Implements Caesar cipher algorithm: all letters a substituted by letter which is shifted by encrypted_key mod 23. Symbols like space and - are not shifted
+        :param message: input plain test message
+        :param encryption_key: int
+        :return: encrypted message
+        """
+        n_letters = 26
+        a = []
+        for l in message:
+            base = ord('a') if l.islower() else ord('A')
+            a.append(chr(base + (ord(l) + encryption_key - base) % n_letters) if l.isalpha() else l)
+        return ''.join(a)
 
-def unscramble(message, words):
-	"""Get a list of english word
-	:param message: scrambled english text where spaces are removed and all letters are moved to different position
-	:param words: list of all English words
+    @staticmethod
+    def random_cipher(message):
+        """ Remove all spaces then shift all letters in message into a random positions
+            :param message: initial message
+            :returns: scrambled message
+        """
+        import random
 
-	"""
-	n_letters = len(message)
-	how_many_samples = 10
-	while True:
-		list_of_lists_of_words = map(lambda x: words_in_string(x), [random_move(string) for y in xrange(some_number)])
+        message = message.replace(' ', '')
+        scrambled_message = list(message)
+        n = len(message)
+        for i in range(n):
+            k = random.randint(i + 1, n)
+            scrambled_message[i] = scrambled_message[k]
+        return scrambled_message
 
-		for s, words in list_of_lists_of_words:
-			if s = len(string):
-		return ‘ ’.join(list_of_words)
+    @staticmethod
+    def words_in_message(message):
+        found_words = [word for word in Decipher.ENGLISH_WORLDS if word in message]  # O(len(words))
+        return sum(map(lambda word: len(word), found_words)), found_words
 
-class TestDecrypt(unittest.TestCase):
-	def setUp(self):
-		super(TestDecrypt, self).setUp()
+    @staticmethod
+    def random_decipher(message):
+        """
+            :param message: scrambled english text where spaces are removed and all letters are moved to random positions e.g. This is a sample -> histsiaampels
+            :returns: a list of all valid English words e.g. [This, is,sample, a, additional]
+    # n_letters = len(message)
+    # how_many_samples = 10
+    # while True:
+    # 	#list_of_lists_of_words = map(lambda x: words_in_string(x), [random_move(string) for y in xrange(some_number)])
+    #
+    # 	for s, words in list_of_lists_of_words:
+    # 		if s == len(message):
+    #           return ''.join(list_of_words)
 
-	def test_normal(self):
-		worlds = ['This', 'is', 'a', 'sample']
-		self.assertEqual(self.values[n], fib(n))
+        """
+        return message
+
+
+class TestDecipher(TestCase):
+    def setUp(self):
+        super(TestDecipher, self).setUp()
+
+    def t1est_random_decipher(self):
+        encrypted = Decipher.random_cipher(message='This is a sample')
+        self.assertEqual(['This', 'is', 'a', 'sample'], Decipher.random_decipher(message=encrypted))
+
+    def test_caesar_cipher(self):
+        self.assertEqual('A', Decipher.caesar_cipher(message='A', encryption_key=0))
+
+        self.assertEqual('z', Decipher.caesar_cipher(message='a', encryption_key=25))
+        self.assertEqual('z', Decipher.caesar_cipher(message='a', encryption_key=51))
+
+        self.assertEqual('a', Decipher.caesar_cipher(message='a', encryption_key=26))
+        self.assertEqual('b', Decipher.caesar_cipher(message='a', encryption_key=27))
+        self.assertEqual('c', Decipher.caesar_cipher(message='a', encryption_key=28))
+        self.assertEqual('okffng-Qwvb', Decipher.caesar_cipher(message='middle-Outz', encryption_key=2))
