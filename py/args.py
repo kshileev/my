184b69ec99e7c28376baf9cfc5f2937427384145
Kirill Shileev <kshileev@gmail.com>
2016-12-07 17:42:19 +0300

    args: added test case

--- py/args.py
+++ py/args.py
@@ -1,9 +1,20 @@
-import argparse
-from log import logger
+from unittest import TestCase
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--foo', required=False, help='foo help')
-parser.add_argument('--all', action='store_true', help='all help')
-parser.add_argument('--str', type=str, nargs='+', required=True, help='<REQUIRED> a list of strings')
-args = parser.parse_args()
-logger.info(args)
+
+def parser_example(args):
+    import argparse
+
+    parser = argparse.ArgumentParser(prog='example')
+    parser.add_argument('--foo', required=False, help='foo help')
+    parser.add_argument('--all', action='store_true', help='all help')
+    parser.add_argument('--str', type=str, choices=['a', 'b', 'c'], nargs='+', required=True, help='<REQUIRED> a list of strings')
+    values = parser.parse_args(args)
+    return values.foo, values.all, values.str
+
+
+class TestParser(TestCase):
+    def setUp(self):
+        super(TestParser, self).setUp()
+
+    def test_parser(self):
+        self.assertEqual((None, False, ['a', 'b', 'c']), parser_example(args=['--str', 'a', 'b', 'c']))
